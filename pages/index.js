4228f3870f3804b22718b4838c4337380877f9bf
Susanta <inbox.susanta@gmail.com>
2022-01-31 21:20:41 +0530

    creating the layout component

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home Page</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
@@ -18,7 +17,6 @@ export default function Home() {
       <Link href="/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
