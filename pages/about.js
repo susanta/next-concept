45c125a980df031203dbbb7aab68ce918180b73b
Susanta <inbox.susanta@gmail.com>
2022-01-31 22:16:14 +0530

    Images & metadata

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,21 @@
+import Head from 'next/head';
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita cumque
-        molestiae tempore incidunt doloremque, culpa illo nisi unde suscipit
-        itaque?
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Next Concept | About</title>
+        <meta name="description" content="About meta description" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          cumque molestiae tempore incidunt doloremque, culpa illo nisi unde
+          suscipit itaque?
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,22 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Home Page</h1>
-      <p className={styles.text}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
-        voluptates vitae inventore quo atque deleniti consectetur unde facilis
-        ad optio officia totam, itaque at modi nisi sed et sint laboriosam?
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Next Concept | Home</title>
+        <meta name="description" content="Home meta description" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home Page</h1>
+        <p className={styles.text}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
+          voluptates vitae inventore quo atque deleniti consectetur unde facilis
+          ad optio officia totam, itaque at modi nisi sed et sint laboriosam?
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
