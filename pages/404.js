5d0ef9c805906eddd3cfff9abbe7f567d43c92dd
Susanta <inbox.susanta@gmail.com>
2022-01-31 21:41:36 +0530

    Redirecting users

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,17 @@
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      //    router.go(1); // 1 go to next -1 go to previous
+      router.push('/');
+    }, 3000);
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Oooops...</h1>
